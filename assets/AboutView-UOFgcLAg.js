5b6bf2782ae836af5a202965e358042e8450f7c3
emillo <yc.gd1688@gmail.com>
2024-02-25 14:37:51 +0800

    Updates

---
+++ assets/AboutView-UOFgcLAg.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,b as o,w as a,d as e,F as r,a as c,r as _,o as u}from"./index-ouduW3sl.js";const d={},i=c("h1",null,"This is About page.",-1);function l(p,f,m,b,h,x){const t=_("RouterLink");return u(),n(r,null,[i,o(t,{to:"/"},{default:a(()=>[e("Home")]),_:1}),e(" | "),o(t,{to:"/about"},{default:a(()=>[e("About")]),_:1})],64)}const V=s(d,[["render",l]]);export{V as default};
